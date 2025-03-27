88818e27e764c32176cf7e345119e36c6f9dd612
tsztin0217 <tsztin0217@gmail.com>
2025-03-27 18:02:12 -0400

    Refactor fetchData and render presentational comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-
-
 import { useState } from 'react'
 import * as weatherService from './services/weatherService';
+import WeatherSearch from './components/WeatherSearch/WeatherSearch';
+import WeatherDetails from './components/WeatherDetails/WeatherDetails';
 import './App.css'
 
 
 
 const App = () => {
-  const fetchData = async () => {
-    const data = await weatherService.show('Novato');
-    console.log('Data: ', data);
+  const [weather, setWeather] = useState({});
+
+  const fetchData = async (city) => {
+    const data = await weatherService.show(city);
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text
+    };
+    setWeather(newWeatherState);
+    // console.log('Data: ', data);
   };
+  console.log('State: ', weather);
   return (
     <main>
       <h1>Weather API</h1>
-      <button onClick={fetchData}>Fetch Weather Data</button>
+      <WeatherSearch fetchData={fetchData} /> 
+      <WeatherDetails weather={weather} />
     </main>
   );
 }

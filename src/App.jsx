ec7fe70d1e38c4cfc5d5a2f470980c57c960026d
tsztin0217 <tsztin0217@gmail.com>
2025-03-27 16:39:51 -0400

    Imports and utilize service function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-// src/App.jsx
+
+
+import { useState } from 'react'
+import * as weatherService from './services/weatherService';
+import './App.css'
+
+
 
 const App = () => {
-
+  const fetchData = async () => {
+    const data = await weatherService.show('Novato');
+    console.log('Data: ', data);
+  };
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </main>
   );
 }
 
 export default App
+
+
 
